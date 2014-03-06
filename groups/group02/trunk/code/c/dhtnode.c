96bf74f02390b09052e016d3b4ca5096b8f1acb7
Henri Hagberg <hmhagberg@gmail.com>
2014-03-06 10:32:08 +0000

    Logic for connecting to the network is ready (implementation is not). Basic structure for new connection handling is ready.

--- groups/group02/trunk/code/c/dhtnode.c
+++ groups/group02/trunk/code/c/dhtnode.c
@@ -8,7 +8,8 @@
    ip(7) */
 
 #include "dhtpackettypes.h"
-#include "create_packet.h"
+#include "dhtpacket.h"
+#include "socketio.h"
 #include <sys/select.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,11 +29,10 @@
 #define SERVER_ADDR "example.com"
 #define SERVER_PORT 1234
 
-struct TCP_addr {
+struct tcp_addr {
+    uint16_t port;
 	char *addr;
-	uint16_t port;
 };
-typedef struct TCP_addr TCP_addr;
 
 void die(char *reason) {
     fprintf(stderr, "Fatal error: %s\n", reason);
@@ -73,9 +73,9 @@ int main(void) {
     int listensock = create_listen_socket();
 
 	int servsock;
-	void *sendbuf;
-	void *rcvbuf;
-	void *payloadbuf;
+	void *sendbuf = malloc(MAX_PACKET_SIZE);
+	void *rcvbuf = malloc(MAX_PACKET_SIZE);
+	void *plbuf = malloc(MAX_PACKET_SIZE);
 	int packetlen;
 	struct addrinfo servhints, hosthints, *servinfo, *hostinfo;
 	memset(&servhints, 0, sizeof(servhints));
@@ -96,14 +96,19 @@ int main(void) {
 	sendall(servsock, DHT_CLIENT_SHAKE, 2, 0);	// Handshake
 	rcvall(servsock, rcvbuf, MAX_PACKET_SIZE, 0);
 
-	TCP_addr tcp_addr = {hostinfo->ai_addr, HOST_PORT};
+	struct tcp_addr host_addr = {hostinfo->ai_addr, HOST_PORT};
 	packetlen = create_packet(&sendbuf, MAX_PACKET_SIZE, servinfo->ai_addr, SERVER_PORT, hostinfo->ai_addr, HOST_PORT,
-	DHT_REGISTER_BEGIN, &tcp_addr, sizeof(uint16_t) + strlen(tcp_addr.addr));
+	DHT_REGISTER_BEGIN, &host_addr, sizeof(uint16_t) + strlen(host_addr.addr));
 	sendall(servsock, sendbuf, packetlen, 0);	// DHT_REGISTER_BEGIN
+
+    // Connection flags
+    int ACKS_RECEIVED = 0;
+    int left = -1;
+    int right = -1;
+    sha1_t nb_hashes[2];
 
     while(running) {
 		FD_ZERO(&rfds);
-		// FD_SET(0, &rfds); /* Standard input */
 		FD_SET(listensock, &rfds);     
 
 		retval = select(listensock + 1, &rfds, NULL, NULL, NULL);
@@ -114,31 +119,110 @@ int main(void) {
 			if (FD_ISSET(listensock, &rfds)) {
 				struct sockaddr_in tempaddr;
 				unsigned int addrlen = 0;
-				int tempfd = accept(listensock, (struct sockaddr *)&tempaddr,
-							&addrlen);
+                int tempfd = accept(listensock, (struct sockaddr *)&tempaddr,
+                            &addrlen);
+                if (left == -1) {
+                    left = tempfd;
+                } else if (left != -1 && right == -1) {
+                    right = tempfd;
+                } else {
+                    // ERROR
+                }
+                FD_SET(tempfd, &rfds);
 				recvall(tempfd, recvbuf, MAX_PACKET_SIZE, 0);
-				Header *header = read_header(recvbuf);
-				if (header->target == this) {	// this == hosthash
-					switch (header->type) {
-						case DHT_CLIENT_SHAKE:
-							break;	// From neighbour (connecting)
-						case DHT_SERVER_SHAKE:
-							break; // From new neighbour
-						case DHT_REGISTER_BEGIN:
-							break; // From server
-						case DHT_REGISTER_ACK:
-							break; // Received data from neighbour (connecting)
-						case DHT_REGISTER_FAKE_ACK:
-							break; // First node in network (connecting)
-						case DHT_REGISTER_DONE:
-							break; // Forget data sent to new neighbour
-					}
-				} else {
-					forward(); // NOT IMPLEMENTED
-				}
-				close(tempfd);
+				struct header *header = read_header(recvbuf);
+                switch (header->type) {
+                    case DHT_CLIENT_SHAKE:
+                        sendall(tempfd, DHT_SERVER_SHAKE, 2, 0);
+                        break;
+                    default:
+                        // ERROR
+                }
 				
-			}
+			} else if (FD_ISSET(left, &rfds) || FD_ISSET(right, &rfds)) {
+                int tempfd;
+                if (FD_ISSET(left, &rfds)) {
+                    tempfd = left;
+                } else {
+                    tempfd = right;
+                }
+                recvall(tempfd, recvbuf, MAX_PACKET_SIZE, 0);
+                struct header *header = read_header(recvbuf);
+                switch (header->type) {
+                    case DHT_TRANSFER_DATA:
+                        read_packet(plbuf, recvbuf, header->pl_len);
+                        insert_data(plbuf, pl_len); // TODO Implement linked list
+                        break;
+                    case DHT_REGISTER_ACK:    // Received data from neighbour (connecting)
+                        nb_hashes[ACKS_RECEIVED] = header->sender;
+                        ACKS_RECEIVED++;
+
+                        if (ACKS_RECEIVED == 2) {
+                            packetlen = create_packet(&sendbuf, MAX_PACKET_SIZE, servinfo->ai_addr, SERVER_PORT, hostinfo->ai_addr, HOST_PORT,
+                            DHT_REGISTER_DONE, &host_addr, sizeof(uint16_t) + strlen(host_addr.addr));
+                            sendall(servsock, sendbuf, packetlen, 0);
+                            
+                            if () {
+                                // Decide who is who
+                            }
+                        }
+                        break; 
+                    case DHT_REGISTER_DONE:   // Forget data sent to new neighbour
+                        break;   
+                    default:
+                        // ERROR
+                }
+
+            } else if (FD_ISSET(servsock, &rfds)) {
+                recvall(servsock, recvbuf, MAX_PACKET_SIZE, 0);
+                struct header *header = read_header(recvbuf);
+                switch (header->type) {
+                    case DHT_REGISTER_FAKE_ACK:   // First node in network (connecting)
+                        break;
+                    case DHT_REGISTER_BEGIN:
+                        int sendleft = 0;
+                        int sendright = 0;
+                        if (left == -1 && right == -1) { // If we are currently the only node
+                            sendleft = 1;
+                            sendright = 1;
+                        } else if (target == left_side) { // If new neighbour is to the left
+                            sendleft = 1;
+                        } else if (target == right_side) { // If new neighbour is to the left
+                            sendright = 1;
+                        }
+
+                        if (sendleft) {
+                            read_packet(plbuf, recvbuf, header->pl_len);
+                            struct tcp_addr *nb_addr = build_tcp_addr(plbuf);
+                            struct addrinfo nb_hints, *nb_info;
+                            memset(&nb_hints, 0, sizeof(nb_hints));
+                            nb_hints.ai_family = AF_UNSPEC;
+                            nb_hints.ai_socktype = SOCK_STREAM;
+                            getaddrinfo(NULL, NULL, &nb_hints, &nb_info);
+                            left = socket(nb_info->ai_family, nb_info->ai_socktype, nb_info->ai_protocol);
+                            connect(left, nb_info->ai_addr, nb_info->ai_addrlen);
+                            
+
+                        }
+
+                        if (sendright) {
+                            read_packet(plbuf, recvbuf, header->pl_len);
+                            struct tcp_addr *nb_addr = build_tcp_addr(plbuf);
+                            struct addrinfo nb_hints, *nb_info;
+                            memset(&nb_hints, 0, sizeof(nb_hints));
+                            nb_hints.ai_family = AF_UNSPEC;
+                            nb_hints.ai_socktype = SOCK_STREAM;
+                            getaddrinfo(NULL, NULL, &nb_hints, &nb_info);
+                            right = socket(nb_info->ai_family, nb_info->ai_socktype, nb_info->ai_protocol);
+                            connect(right, nb_info->ai_addr, nb_info->ai_addrlen);
+                        }
+                        break;
+                    case DHT_REGISTER_DONE:   // Forget data sent to new neighbour
+                        break;   
+                    default:
+                        // ERROR
+                }
+            }
 		}
     }
 
--- groups/group02/trunk/code/c/dhtpacket.h
+++ groups/group02/trunk/code/c/dhtpacket.h
@@ -9,19 +9,20 @@
 #include <errno.h>
 #include <string.h>
 
-struct Header {
+struct header {
 	sha1_t target;
-	sha1_t header;
+	sha1_t sender;
 	uint16_t type;
 	uint16_t pl_len;
 };
-typedef struct Header Header;
 
 int create_packet(void **buf, int buflen, char *target_addr, uint16_t target_port, char *sender_addr, uint16_t sender_port, uint16_t type, void *payload, uint16_t payload_len);
 
-int read_packet(void **buf, void *packet);
+int read_packet(void **buf, void *packet, uint16_t pl_len);
 
-Header* read_header(void *packet);
+struct header* read_header(void *packet);
+
+struct tcp_addr* build_tcp_addr(void *payload);
 
 
 
