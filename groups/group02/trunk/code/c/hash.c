7eadfb5383e7a81d10c0a77267b24b59594ad6cf
Henri Hagberg <hmhagberg@gmail.com>
2014-03-24 08:57:16 +0000

    Fixed makefile.

--- groups/group02/trunk/code/c/hash.c
+++ groups/group02/trunk/code/c/hash.c
@@ -1,8 +1,8 @@
 #include "typedefs.h"
 #include <openssl/sha.h>
 
-int hash_addr(struct tcp_addr *tcp_addr, sha1_t key) {
+int hash_addr(struct tcp_addr *tcp_addr, sha1_t *key) {
     const unsigned char *temp = (unsigned char *) tcp_addr->addr;
-    SHA1(temp, strlen(tcp_addr->addr), key);
+    SHA1(temp, strlen(tcp_addr->addr), *key);
     return 0;
 }
