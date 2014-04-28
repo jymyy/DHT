98fc4f05ced4c425525aa30e9aad92666c17fcd5
294049 <rantalj7@gmail.com>
2014-04-28 11:20:07 +0000

    Seperating progressbar and GUI

--- groups/group02/trunk/code/java/src/dht/GUI.java
+++ groups/group02/trunk/code/java/src/dht/GUI.java
@@ -230,6 +230,7 @@ public class GUI extends JFrame {
 					controller = new DHTController(haddr, hport, saddr, sport);
 					connected = 1;
 					directory(controller.getDHTdir());
+					JOptionPane.showMessageDialog(null, "Connection completed.");
 	
 				} catch (Exception ex) {
 					JOptionPane.showMessageDialog(null, "Connection to server failed. Be sure your ports and addresses are correct.");
@@ -290,60 +291,7 @@ public class GUI extends JFrame {
 			}
 		}
 	
-	// Progress bar
-	
-	public class Progressbar {
-		JProgressBar progressBar;
-		int maxValue;
-		int init;
-		String status;
-	    JFrame progFrame;
-	    JPanel progPanel;
-	    JLabel textLabel;
-		
-		Progressbar(int init, int maxValue, String status) {
-			this.init = init;
-			this.maxValue = maxValue;
-			this.status = status;
-			this.textLabel = new JLabel();
-			this.textLabel.setBounds(10,10,100,20);
-			this.textLabel.setText(status);
-			this.progFrame = new JFrame();
-			this.progFrame.setSize(250, 150);
-			this.progFrame.setLocationRelativeTo(null);
-			this.progPanel = new JPanel();
-			this.progPanel.setBorder(BorderFactory.createEmptyBorder(40,40,40,40));
-			this.progPanel.setLayout(new BoxLayout(progPanel, BoxLayout.Y_AXIS));
-			this.progressBar = new JProgressBar(this.init, this.maxValue);
-			this.progressBar.setSize(150, 40);
-			this.progressBar.setAlignmentX(0f);
-			this.progressBar.setValue(this.init);
-			this.progressBar.setStringPainted(true);
-			this.progFrame.add(this.progPanel);
-			this.progPanel.add(this.progressBar);
-			this.progPanel.add(this.textLabel);
-			if (this.init < this.maxValue) {
-				this.progFrame.setVisible(true);
-			}
-			else {
-				this.progFrame.setVisible(false);
-				JOptionPane.showMessageDialog(null, status + " completed.");
-				
-			}
-		}
-		
-		public void update(int progress) {
-			this.progressBar.setValue(progress);
-			this.progressBar.setStringPainted(true);
-			if (progress < this.maxValue) {
-				this.progFrame.setVisible(true);
-			}
-			else {
-				this.progFrame.setVisible(false);
 
-			}
-		}
-	}
 	
 	
 	public void directory(String[] list) {
@@ -542,6 +490,4 @@ public class GUI extends JFrame {
 	}
 	
 }
-
-
 
